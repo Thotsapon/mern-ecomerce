1058627245ef6e4f3455452f9ad854b289fe46bb
Thosapol <aoza00123@gmail.com>
2025-11-07 20:26:25 +0700

    Pro ject compleated

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-	baseURL: import.meta.mode === "development" ? "https://mern-ecomerce.pages.dev/api" : "/api",
+	baseURL: import.meta.mode === "development" ? `${process.env.CLIENT_URL}/api` : "/api",
 	withCredentials: true, // send cookies to the server
 });
 
